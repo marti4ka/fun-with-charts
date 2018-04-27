bb77ae556d4831bbb3b151bd81c9dfc4b56a9094
Martina Dukadinova <marti4ka@users.noreply.github.com>
2018-04-27 16:24:39 +0200

    Update BMI.js

--- exercises/BMI.js
+++ exercises/BMI.js
@@ -2,7 +2,8 @@ app.factory('BMI', function() {
     return {
       getOptions: function() {
         var options = {
-          height: 400, width: 900
+          height: 400, width: 900,
+          colorAxis: {colors: ['green', 'red']}
         };
         return options;
       },
