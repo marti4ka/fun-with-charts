813781f17ebe6badf379e6868873e96e1ef0fe72
Briana Swift <brianamarie@github.com>
2018-04-27 16:36:13 +0200

    change semicolons to commas

--- exercises/timeline.js
+++ exercises/timeline.js
@@ -2,8 +2,8 @@ app.factory('timeline', function() {
     return {
         getOptions: function() {
           var options = {
-            height: 400;
-            width: 400;  
+            height: 400,
+            width: 400,
             timeline: { singleColor: '#8d8' }
           };
           return options;
