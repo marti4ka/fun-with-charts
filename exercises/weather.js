029074e641b2d7ca71ca7ac537fc87a1b4b88008
Hector Alfaro <hectorsector@github.com>
2018-04-27 10:35:51 -0400

    make it bright

--- exercises/weather.js
+++ exercises/weather.js
@@ -2,8 +2,7 @@ app.factory('weather', function() {
     return {
         getOptions: function() {
             var options = {
-              // Insert options here
-              // refer to: https://developers.google.com/chart/interactive/docs/gallery/linechart
+                backgroundColor: yellow
             };
             return options;
         },
